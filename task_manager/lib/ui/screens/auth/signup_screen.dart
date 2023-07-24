6d12c3c187bade4d8b3921cf5f2bb64b60cc7767
Mahibul Hassan <mahibulhassan@Mahibuls-MacBook-Pro.local>
2023-07-25 00:36:56 +0600

    auth screen done and verification screen done

---
+++ task_manager/lib/ui/screens/auth/signup_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/ui/widgets/screen_background.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ScreenBackground(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 64,),
+                  Text(
+                    'Join With Us',
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .titleLarge,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  const TextField(
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      hintText: 'First name',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  const TextField(
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      hintText: 'Last name',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  const TextField(
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                      hintText: 'Mobile',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Icon(Icons.arrow_forward_ios)),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Have an account?",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, letterSpacing: 0.5),
+                      ),
+                      TextButton(onPressed: () {
+                        Navigator.pop(context);
+                      }, child: const Text('Sign in')),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
